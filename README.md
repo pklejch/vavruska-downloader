2838314d508c74efd0b73039ee9bb7cf150e9617
Petr Klejch <pklejch@seznam.cz>
2023-04-05 21:57:08 +0200

    Initial commit with code.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vavruska downloader
+
+Script to download all dancing videos from Vavruska.
+
+## HOW TO
+1. Create file `passwords` and fill passwords from emails. **You have to use your own passwords.** One password = one line.
+```
+password-for-course-1
+password-for-course-2
+password-for-course-3
+```
+2. Create venv and install requirements
+```python
+python3 -m venv venv
+. venv/bin/activate
+```
+3. Run script
+```
+python run.py
+```
+4. By default your videos will be in the `videos` folder in the current working directory.
---
+++ run.py
@@ -0,0 +1,62 @@
+import requests
+import bs4
+import yt_dlp
+
+VAVRUSKA_SYLABUS_URL = 'https://www.vavruska.info/video-sylabus/?action=login'
+PASSWORD_FILE = 'passwords'
+OUTPUT_FOLDER = 'videos'
+
+
+class VavruskaDownloader:
+    """Download Vavruska YT videos."""
+
+    def __init__(self, passwords_filename: str, output_folder: str):
+        self.passwords_filename = passwords_filename
+        self.output_folder = output_folder
+
+        self.passwords: list[str] = []
+        self._load_passwords()
+
+    def _load_passwords(self) -> None:
+        with open(self.passwords_filename) as file:
+            for line in file.readlines():
+                line = line.strip()
+                self.passwords.append(line)
+
+    def download(self) -> None:
+        """Download all YT videos from syllabus."""
+        youtube_links_to_download = []
+
+        for password in self.passwords:
+            youtube_links_to_download.extend(self._get_links(password))
+
+        self._download_video(youtube_links_to_download)
+
+    def _get_links(self, password: str) -> list[str]:
+        session = requests.sessions.Session()
+        response = session.post(
+            VAVRUSKA_SYLABUS_URL,
+            data={'password': password}
+        )
+
+        parsed_page = bs4.BeautifulSoup(response.text, 'html.parser')
+        youtube_links_to_download = []
+
+        for iframe in parsed_page.find_all('iframe'):  # YT videos are iframes
+            youtube_link = iframe.get('src')
+
+            youtube_links_to_download.append(youtube_link)
+
+        return youtube_links_to_download
+
+    def _download_video(self, youtube_links: list[str]) -> None:
+        ydl_opts = {
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+            'outtmpl': f'{OUTPUT_FOLDER}/%(title)s.%(ext)s'
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(youtube_links)
+
+
+vd = VavruskaDownloader(PASSWORD_FILE, OUTPUT_FOLDER)
+vd.download()
